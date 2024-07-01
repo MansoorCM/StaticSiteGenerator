ed054c64d2b0763fc7823b728bc8c05db171acbd
MansoorCM <manzoorcm96@gmail.com>
2024-07-01 12:47:07 +0530

    Added function to convert textnode to htmlnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from leafnode import LeafNode
+
 class TextNode():
     def __init__(self, text, text_type, url = None):
         self.text = text
@@ -9,3 +11,18 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def textnode_to_htmlnode(textnode):
+    if textnode.text_type == 'text':
+        return LeafNode(None, textnode.text)
+    if textnode.text_type == 'bold':
+        return LeafNode('b', textnode.text)
+    if textnode.text_type == 'italic':
+        return LeafNode('i', textnode.text)
+    if textnode.text_type == 'code':
+        return LeafNode('code', textnode.text)
+    if textnode.text_type == 'link':
+        return LeafNode('a', textnode.text, {'href':textnode.url})
+    if textnode.text_type == 'image':
+        return LeafNode('img', '', {'src':textnode.url, 'alt':textnode.text})
+    raise Exception('invalid text type')
