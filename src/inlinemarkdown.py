46f81fcd472f6171f6fb63b88349a036960dc0fc
MansoorCM <manzoorcm96@gmail.com>
2024-07-11 12:51:04 +0530

    Added function and tests to split a raw text to different types of nodes

--- src/inlinemarkdown.py
+++ src/inlinemarkdown.py
@@ -1,6 +1,9 @@
 from textnode import (
     TextNode,
     text_type_text,
+    text_type_code,
+    text_type_bold,
+    text_type_italic
     )
 import re
 
@@ -29,3 +32,45 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     pattern = r'\[(.*?)\]\((.*?)\)'
     return re.findall(pattern, text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == text_type_text:
+            alt_links = extract_markdown_images(node.text)
+            text = node.text
+            for alt, link in alt_links:
+                part, text = text.split(f"![{alt}]({link})", 1)
+                if part != '':
+                    new_nodes.append(TextNode(part, text_type_text))
+                new_nodes.append(TextNode(alt, 'image', link))
+            if text != '':
+                new_nodes.append(TextNode(text, text_type_text))
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == text_type_text:
+            anchor_links = extract_markdown_links(node.text)
+            text = node.text
+            for anchor, link in anchor_links:
+                part, text = text.split(f"[{anchor}]({link})", 1)
+                if part != '':
+                    new_nodes.append(TextNode(part, text_type_text))
+                new_nodes.append(TextNode(anchor, 'link', link))
+            if text != '':
+                new_nodes.append(TextNode(text, text_type_text))
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    new_nodes = split_nodes_delimiter(nodes, '`', text_type_code)
+    new_nodes = split_nodes_delimiter(new_nodes, '**', text_type_bold)
+    new_nodes = split_nodes_delimiter(new_nodes, '*', text_type_italic)
+    new_nodes = split_nodes_image(new_nodes)
+    return split_nodes_link(new_nodes)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -2,12 +2,19 @@ import unittest
 from textnode import (
     TextNode,
     text_type_text,
-    text_type_code
+    text_type_code,
+    text_type_image,
+    text_type_link,
+    text_type_bold,
+    text_type_italic
     )
 from inlinemarkdown import (
     split_nodes_delimiter,
     extract_markdown_images,
-    extract_markdown_links
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes
 )
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -32,6 +39,59 @@ class TestInlineMarkdown(unittest.TestCase):
         expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
         anchor_links = extract_markdown_links(text)
         self.assertEqual(expected, anchor_links)
+
+    def test_inline_markdown_split_nodes_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            text_type_text,
+            )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a link ", text_type_text),
+            TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+            TextNode(" and ", text_type_text),
+            TextNode(
+                "to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"
+            ),
+            ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_inline_markdown_split_nodes_image(self):
+        node = TextNode(
+                "This is text with a link ![to boot dev](https://www.boot.dev/img/bootdev-logo-full-small.webp) " + 
+                "and ![to youtube](https://t3.ftcdn.net/jpg/04/74/05/94/360_F_474059464_qldYuzxaUWEwNTtYBJ44VN89ARuFktHW.jpg)",
+                text_type_text,
+                )
+        new_nodes = split_nodes_image([node])
+        expected = [
+                TextNode("This is text with a link ", text_type_text),
+                TextNode("to boot dev", text_type_image, "https://www.boot.dev/img/bootdev-logo-full-small.webp"),
+                TextNode(" and ", text_type_text),
+                TextNode(
+                        "to youtube", text_type_image, "https://t3.ftcdn.net/jpg/04/74/05/94/360_F_474059464_qldYuzxaUWEwNTtYBJ44VN89ARuFktHW.jpg"
+                    ),
+                ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_text_to_textnode(self):
+        text = ('This is **text** with an *italic* word and a `code block` and an ' + 
+            '![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) ' + 
+            'and a [link](https://boot.dev)'
+            )
+        nodes = text_to_textnodes(text)
+        expected = [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ]
+        self.assertEqual(expected, nodes)
 
 if __name__ == "__main__":
     unittest.main()
