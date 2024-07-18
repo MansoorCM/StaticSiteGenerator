1392ec489aa0daa07e33b279338cdf2d664862ac
MansoorCM <manzoorcm96@gmail.com>
2024-07-18 12:01:08 +0530

    Added function to get block type from block

--- src/blockmarkdown.py
+++ src/blockmarkdown.py
@@ -1,3 +1,10 @@
+block_type_paragraph = 'paragraph'
+block_type_heading = 'heading'
+block_type_code = 'code'
+block_type_quote = 'quote'
+block_type_unordered_list = 'unordered_list'
+block_type_ordered_list = 'ordered_list'
+
 def markdown_to_block(string):
     filtered_blocks = []
     for line in string.split("\n\n"):
@@ -6,3 +13,36 @@ def markdown_to_block(string):
             continue
         filtered_blocks.append(modified)
     return filtered_blocks
+
+def block_to_block_type(block):
+    if (block.startswith('# ') or 
+        block.startswith('## ') or 
+        block.startswith('### ') or 
+        block.startswith('#### ') or 
+        block.startswith('##### ') or 
+        block.startswith('###### ')):
+        return block_type_heading
+    lines = block.split('\n')
+    if lines[0].startswith('```') and lines[-1].endswith('```'):
+        return block_type_code
+    if lines[0].startswith('>'):
+        return is_block_of_given_type(lines, '>', block_type_quote)
+    if lines[0].startswith('*'):
+        return is_block_of_given_type(lines, '* ', block_type_unordered_list)
+    if lines[0].startswith('-'):
+        return is_block_of_given_type(lines, '- ', block_type_unordered_list)
+    if is_block_of_type_orderedlist(lines):
+        return block_type_ordered_list
+    return block_type_paragraph
+
+def is_block_of_given_type(lines, pattern, block_type):
+    for i in range(len(lines)):
+        if not lines[i].startswith(pattern):
+            return block_type_paragraph
+    return block_type
+
+def is_block_of_type_orderedlist(lines):
+    for i in range(len(lines)):
+        if not (lines[i].startswith(f"{i + 1}. ")):
+            return False
+    return True
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,14 @@
 import unittest
-from blockmarkdown import markdown_to_block
+from blockmarkdown import (
+    markdown_to_block,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_code,
+    block_type_heading,
+    block_type_ordered_list,
+    block_type_unordered_list,
+    block_type_quote,
+    )
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_block(self):
@@ -19,6 +28,29 @@ This is the same paragraph on a new line
             ]
         self.assertEqual(expected, markdown_to_block(md))
 
+    def test_block_to_block_type_paragraph(self):
+        block = "This is **bolded** paragraph"
+        self.assertEqual(block_type_paragraph, block_to_block_type(block))
+    
+    def test_block_to_block_type_code(self):
+        block = "```This is a code block```"
+        self.assertEqual(block_type_code, block_to_block_type(block))
+
+    def test_block_to_block_type_heading(self):
+        block = "# This is a heading"
+        self.assertEqual(block_type_heading, block_to_block_type(block))
+
+    def test_block_to_block_type_quote(self):
+        block = ">this is quote 1\n>this is quote 2\n>this is quote 3"
+        self.assertEqual(block_type_quote, block_to_block_type(block))
+
+    def test_block_to_block_type_unorderedlist(self):
+        block = "* this is item 1\n* this is item 2\n* this is item 3"
+        self.assertEqual(block_type_unordered_list, block_to_block_type(block))
+
+    def test_block_to_block_type_orderedlist(self):
+        block = "1. this is item 1\n2. this is item 2\n3. this is item 3"
+        self.assertEqual(block_type_ordered_list, block_to_block_type(block))
 
 if __name__== "__main__":
     unittest.main()
