0c4f0c3dca9109075eafcace20529f1e0fcd5275
MansoorCM <manzoorcm96@gmail.com>
2024-07-30 12:00:59 +0530

    Added function to convert markdown to html node

--- src/blockmarkdown.py
+++ src/blockmarkdown.py
@@ -1,3 +1,6 @@
+from leafnode import LeafNode
+from parentnode import ParentNode
+
 block_type_paragraph = 'paragraph'
 block_type_heading = 'heading'
 block_type_code = 'code'
@@ -46,3 +49,46 @@ def is_block_of_type_orderedlist(lines):
         if not (lines[i].startswith(f"{i + 1}. ")):
             return False
     return True
+
+def markdown_to_htmlnode(string):
+    blocks = markdown_to_block(string)
+    children = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_paragraph:
+            children.append(LeafNode('p', block))
+        elif block_type == block_type_heading:
+            children.append(block_heading_to_html(block))
+        elif block_type == block_type_code:
+            children.append(block_code_to_html(block))
+        elif block_type == block_type_quote:
+            children.append(block_quote_to_html(block))
+        elif block_type == block_type_unordered_list:
+            children.append(block_list_to_html(block, 'ul'))
+        elif block_type == block_type_ordered_list:
+            children.append(block_list_to_html(block, 'ol'))
+        else:
+            raise ValueError("invalid block type")
+    return ParentNode('div', children)
+
+def block_heading_to_html(block):
+    idx = block.find(' ')
+    return LeafNode(f"h{idx}", block[idx + 1:])
+
+def block_code_to_html(block):
+    html_code_node = LeafNode('code', block[3:-3])
+    return ParentNode('pre', [html_code_node])
+
+def block_quote_to_html(block):
+    filtered = []
+    for line in block.split('\n'):
+        filtered.append(line[1:])
+    block = '\n'.join(filtered)
+    return LeafNode('blockquote', block)
+
+def block_list_to_html(block, tag):
+    children = []
+    for line in block.split('\n'):
+        idx = line.find(' ')
+        children.append(LeafNode('li', line[idx + 1:]))
+    return ParentNode(tag, children)
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,7 +1,10 @@
 import unittest
+from leafnode import LeafNode
+from parentnode import ParentNode
 from blockmarkdown import (
     markdown_to_block,
     block_to_block_type,
+    markdown_to_htmlnode,
     block_type_paragraph,
     block_type_code,
     block_type_heading,
@@ -51,6 +54,25 @@ This is the same paragraph on a new line
     def test_block_to_block_type_orderedlist(self):
         block = "1. this is item 1\n2. this is item 2\n3. this is item 3"
         self.assertEqual(block_type_ordered_list, block_to_block_type(block))
+
+    def test_markdown_to_html(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        list_nodes = [LeafNode('li', "This is a list"), LeafNode('li', "with items")] 
+        children = [
+                LeafNode('p', "This is **bolded** paragraph"),
+                LeafNode('p', "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line"),
+                ParentNode('ul', list_nodes)
+            ]
+        parent = ParentNode('div', children).props_to_html()
+        self.assertEqual(parent, markdown_to_htmlnode(md).props_to_html())
 
 if __name__== "__main__":
     unittest.main()
